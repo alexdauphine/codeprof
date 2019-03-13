ca6cdbd891591e6a2edc657bdf4d1e9c7bf1f10e
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-13 11:29:49 +0100

    Add files via upload

---
+++ src/test/java/io/github/oliviercailloux/sample_jax_rs/HelloTest.java
@@ -0,0 +1,19 @@
+package io.github.oliviercailloux.sample_jax_rs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = Logger.getLogger(HelloTest.class.getCanonicalName());
+
+	@Test
+	public void testServlet() throws Exception {
+		assertEquals("Hello, world.", new Hello().justSayHello());
+	}
+
+}
